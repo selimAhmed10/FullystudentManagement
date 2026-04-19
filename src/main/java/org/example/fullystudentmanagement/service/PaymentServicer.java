63ed34210bbad74477773d13ee10a3c6a1cbaceb
ZBOOK <selim174662@gmail.com>
2026-04-20 00:47:53 +0600

    Updated PaymentServicer

---
+++ src/main/java/org/example/fullystudentmanagement/service/PaymentServicer.java
@@ -0,0 +1,40 @@
+package org.example.fullystudentmanagement.service;
+
+import org.example.fullystudentmanagement.model.Course;
+import org.example.fullystudentmanagement.model.Payment;
+import org.example.fullystudentmanagement.model.User;
+import org.example.fullystudentmanagement.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentServicer {
+    @Autowired
+    private PaymentRepository paymentRepository;
+    public PaymentServicer(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+    public List<Payment> findAll() {
+        return paymentRepository.findAll();
+    }
+
+    public List<Payment> getPaymentsByStudent(User student)
+    {
+        return paymentRepository.findByStudent(student);
+    }
+
+    public List<Payment> getPaymentByCourse(Course course)
+    {
+        return paymentRepository.findByCourse(course);
+    }
+
+    public Payment updatePaymentStatus(Long paymentId,String status)
+    {
+        Payment payment=paymentRepository.findById(paymentId).orElseThrow(() ->new RuntimeException("Payment Not Found"));
+        payment.setStatus(status);
+        return paymentRepository.save(payment);
+
+    }
+}

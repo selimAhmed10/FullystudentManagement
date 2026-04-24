b59e10bcfe79c2409eb99cc8424f956b3d3ca4b1
Selim Ahmed <selim174662@gmail.com>
2026-04-24 13:56:04 +0600

    update the main file

--- src/main/java/org/example/fullystudentmanagement/FullystudentManagementApplication.java
+++ src/main/java/org/example/fullystudentmanagement/FullystudentManagementApplication.java
@@ -1,7 +1,13 @@
 package org.example.fullystudentmanagement;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+
 
 @SpringBootApplication
 public class FullystudentManagementApplication {

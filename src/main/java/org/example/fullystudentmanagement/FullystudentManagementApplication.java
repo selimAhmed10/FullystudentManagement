bd0edcc49d56dce4e7eaee5225bd35f6c91b89fb
Selim Ahmed <selim174662@gmail.com>
2026-04-28 23:38:16 +0600

    update the main class with some annotation

--- src/main/java/org/example/fullystudentmanagement/FullystudentManagementApplication.java
+++ src/main/java/org/example/fullystudentmanagement/FullystudentManagementApplication.java
@@ -1,12 +1,16 @@
 package org.example.fullystudentmanagement;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
+@ComponentScan
+@AutoConfigureAfter({DataSourceAutoConfiguration.class})
 
 
 @SpringBootApplication
